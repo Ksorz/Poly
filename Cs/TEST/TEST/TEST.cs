8c102a8974a3e6ee1a3c419534efd8cfef61b6e8
ksorz <ksorzz@gmail.com>
2020-02-05 10:07:03 +0300

    1

--- Cs/TEST/TEST/TEST.cs
+++ Cs/TEST/TEST/TEST.cs
@@ -10,31 +10,77 @@ namespace TEST
     {
         static void Main(string[] args)
         {
-            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            var colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            byte value = 0;
 
 
-            Console.ResetColor();
-            Console.WriteLine(colors.GetType().GetElementType());
-            var kale = Kaleidoscope(colors, 5);
-            int i = 0;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Введите половину стороны квадрата (от 3 до 20)");
+                    value = Convert.ToByte(Console.ReadLine());
+                    if (value < 3 || value > 20)
+                    {
+                        Console.WriteLine("Ошибочка!");
+                        continue;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибочка!");
+                    continue;
+                }
 
-            foreach (var color in kale)
+                var kale = Kaleidoscope(colors, value);
+                Mirror(kale);
+                Console.ResetColor();
+                Console.WriteLine("Выход: Escape");
+
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            
+
+            
+            
+
+            
+            //var kale = Kaleidoscope(colors, value);
+
+            //Mirror(kale);
+            //Console.ResetColor();
+        }
+
+        static void Mirror(ConsoleColor[,] picture)
+        {
+            int len = picture.GetLength(0);
+            var mirrSquare = new ConsoleColor[len * 2, len * 2];
+            int bigLen = mirrSquare.GetLength(0);
+            int index = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < len; j++)
+                {
+                    mirrSquare[i, j] = picture[i, j];
+                    mirrSquare[i, bigLen - (j + 1)] = picture[i, j];
+                    mirrSquare[bigLen - (i + 1), j] = picture[i, j];
+                    mirrSquare[bigLen - (i + 1), bigLen - (j + 1)] = picture[i, j];
+                }
+            }
+            foreach (var color in mirrSquare)
             {
                 Console.BackgroundColor = color;
-                Console.Write(" ", color);
-                i++;
-                if (i % 5 == 0) Console.WriteLine();
+                Console.Write(" ");
+                index++;
+                if (index % bigLen == 0) Console.WriteLine();
             }
-
-            Console.ResetColor();
-            Console.WriteLine(kale.Length);
         }
-        static ConsoleColor[,] Kaleidoscope(ConsoleColor[] colors, int value)
-        {
-            ConsoleColor[,] arr = new ConsoleColor[value,value];
-            Random rnd = new Random();
 
-            //int range = (value / 2) * (value + 1);
+        static ConsoleColor[,] Kaleidoscope(ConsoleColor[] colors, byte value)
+        {
+            var arr = new ConsoleColor[value,value];
+            Random rnd = new Random();
 
             for (int i = 0; i < value; i++)
             {
