f980aea86636330e84cd63d65903730a2592b9a7
Ksorz <ksorzz@gmail.com>
2020-06-14 03:46:57 +0300

    1

---
+++ (OpenEdu) C#/2 Billiards/BilliardsTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        /* Реализуйте метод для расчета угла отскока шарика от стены. Считайте, что угол падения равен углу отражения, 
+        то есть можно пренебречь всеми физическими эффектами, связанными с кручением шаров, трением шара об стенку и т.п. */
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="directionRadians">Угол направелния движения шара</param>
+        /// <param name="wallInclinationRadians">Угол</param>
+        /// <returns></returns>
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            double final = (2 * wallInclinationRadians) - directionRadians;
+            return final;
+        }
+    }
+}
---
+++ test Py.py
@@ -0,0 +1,5 @@
+print(0 ** 0)
+i = 7
+d = 22.5
+a = i + d
+print(a)
