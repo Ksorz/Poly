b87dfc3d5ed180c32357aa6e535a696e8392f466
Lis <Ksorzz@gmail.com>
2020-11-01 20:56:31 +0300

    1

--- test Py.py
+++ test Py.py
@@ -1,5 +1,23 @@
-print(0 ** 0)
-i = 7
-d = 22.5
-a = i + d
-print(a)
+import math
+n = 4
+k = 3
+l = 0
+for i in range(round(math.factorial(n)/(math.factorial(k)*math.factorial((n-k))))):
+    if i % k == 0:
+        l += 1
+    d = 0
+    for j in range(k):
+
+        print(j + l - 1 + d, end=' ')
+        d += 0
+    print()
+
+
+
+
+
+
+
+
+
+    print(i+1)
