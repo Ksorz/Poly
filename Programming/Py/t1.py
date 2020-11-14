75d2d6e3d6ca470315b84fba170d7a49a41d5e1b
Ksorz <ksorzz@gmail.com>
2020-11-14 13:41:49 +0300

    1

---
+++ Programming/Py/t1.py
@@ -0,0 +1,7 @@
+n = 18
+k = 2
+
+for i in range(n):
+    for j in range(n):
+        if i < j:
+            print(i + 1, j + 1)
--- test Py.py
+++ test Py.py
@@ -1,23 +1,68 @@
 import math
-n = 4
+
+def IsConditionTrue(list):
+
+    count = 0
+
+    for i in list:
+        for j in range(len(list)):
+            if j < len(x) and list[j] < list[j + 1]:
+                count += 1
+            else: break
+
+
+
+def CreateNewList(k, list):
+    list.append({})
+
+def RecursiveFilling(n, k, list):
+
+    if len(list) == 0:
+        for i in range(k): list[i].append(list.append(i + 1))
+
+
+    for i in range(n):
+
+
+
+
+
+n = 18
 k = 3
-l = 0
-for i in range(round(math.factorial(n)/(math.factorial(k)*math.factorial((n-k))))):
-    if i % k == 0:
-        l += 1
-    d = 0
-    for j in range(k):
+lisst = []
 
-        print(j + l - 1 + d, end=' ')
-        d += 0
-    print()
+if len(lisst) == 0:
+    lisst[0] = []
+
+lisst
 
 
-
-
+for i in range(n + 1 - k):
+    list.append()
+    for j in range(n + 1 - k):
+        for p in range(n + 1 - k):
 
 
 
 
+lisst = list(list())
+len(lisst)
 
-    print(i+1)
+for i in range(10):
+    lisst.append(i + 1)
+    for j in range(10):
+        lisst[]
+    print(lisst[i])
+
+
+
+
+
+
+
+
+
+
+
+
+for i in range(math.factorial(n) / (math.factorial(k) * math.factorial(n - k))):
