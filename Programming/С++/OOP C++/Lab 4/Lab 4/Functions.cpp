9051efab39c15dac59770b142a205676893a1072
Lis <Ksorzz@gmail.com>
2020-12-28 00:56:34 +0300

    1

--- Programming/С++/OOP C++/Lab 4/Lab 4/Functions.cpp
+++ Programming/С++/OOP C++/Lab 4/Lab 4/Functions.cpp
@@ -23,60 +23,7 @@ bool yesNo(char yes)
 	else return false;
 }
 
-NSet::NSet()
-{
-	cout << "Укажите количество элементов множества" << endl;
-	cin >> size;
-	set = new int[size];
-	system("cls");
 
-	if (size > 5 && size <= 10)
-	{
-		cout << "Заполнить случайными значениями?" << endl;
-		if (yesNo('y')) randomSetFilling(set, size);
-		system("cls");
-	}
-	else if (size > 10)
-	{
-		cout << "Довольно большое множество, лучше заполнить случайными значениями." << endl;
-		randomSetFilling(set, size);
-		system("cls");
 
-	}
-	else
-	{
-		for (int i = 0; i < size; i++)
-		{
-			cout << "Элемент " << i + 1 << ":" << endl;
-			cin >> set[i];
-			system("cls");
-		}
-	}
-}
 
-void NSet::show()
-{
-	if (size <= 600)
-	{
-		cout << "-- ";
-		for (int i = 0; i < size; i++)
-		{
-			cout << set[i] << " -- ";
-			if ((i + 1) % 15 == 0) cout << endl << "-- ";
-		}
-		cout << endl;
-	}
-	else { cout << "Слишком большое множество, показывать не будем!" << endl; }
-}
 
-void NSet::randomSetFilling(int* arr, const int size)
-{
-	int min, max;
-	cout << "Укажите диапозон значений:" << endl;
-	cin >> min >> max;
-
-	for (int i = 0; i < size; i++)
-	{
-		arr[i] = rnd(min, max);
-	}
-}
--- Programming/С++/OOP C++/Lab 4/Lab 4/Functions.hpp
+++ Programming/С++/OOP C++/Lab 4/Lab 4/Functions.hpp
@@ -3,14 +3,3 @@
 int rnd(int min, int max);
 bool yesNo(char yes);
 
-class NSet
-{
-public:
-	NSet();
-	void show(); //	
-	void randomSetFilling(int* arr, const int size); // «аполнение массива случайными значени€ми
-
-private:
-	int size;
-	int* set;
-};
